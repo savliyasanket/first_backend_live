694fa833df9e2e648d5bcf2c76bbb55bf9557046
savliyasanket <savliyasanket4@gmail.com>
2025-10-06 09:22:32 +0530

    solve error

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,10 +1,8 @@
-
 const express = require("express");
 const router = express.Router();
-const {register,login} = require("../contorllers/Auth")
+const {register, login} = require("../contorllers/Auth");
 
+router.post("/register", register);
+router.post("/login", login);
 
-router.post("/register" , register)
-router.post("/login", login)
-
-module.exports = router
+module.exports = router;
